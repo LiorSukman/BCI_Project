fd443c40afb82c900a7ea7d7073ad5af48d00b8f
TaliRemenick <73112195+TaliRemenick@users.noreply.github.com>
2021-12-13 10:38:41 +0200

    added all_MI

---
+++ all_MI.m
@@ -0,0 +1,7 @@
+% ToolboxFolder - includes the EEGLAB folder
+% destFolder - destination folder where all the files will be saved
+
+function SelectedFeatureNames = all_MI(ToolboxFolder,recordingFolder,destFolder)
+MI2_preprocess(recordingFolder,ToolboxFolder,destFolder);
+MI3_segmentation(destFolder);
+SelectedFeatureNames = MI4_featureExtraction(destFolder);
